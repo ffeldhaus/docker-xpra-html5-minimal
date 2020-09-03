85ca69bcf8c8ad8a2241bcc482527e2e67a67567
Florian Feldhaus <florian.feldhaus@gmail.com>
2020-09-03 22:30:28 +0000

    Added dbus configuration to docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# configure dbus
+dbus-uuidgen > /var/lib/dbus/machine-id
+mkdir -p /var/run/dbus
+dbus-daemon --config-file=/usr/share/dbus-1/system.conf --print-address
+
 exec /usr/bin/xpra start --daemon=no --start-child="$@"
